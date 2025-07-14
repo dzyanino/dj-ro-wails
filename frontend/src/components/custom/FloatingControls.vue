2d791903efbed7064f685d4f0aa599a34389bb36
dzyanino <dzyanino@gmail.com>
2025-07-15 01:22:38 +0300

    Add : graph generation call for random graph

--- frontend/src/components/custom/FloatingControls.vue
+++ frontend/src/components/custom/FloatingControls.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts">
-import { onMounted, shallowRef, nextTick } from 'vue'
-import { CirclePlusIcon, PanelRightClose, PanelRightOpen, ShuffleIcon, SplineIcon } from 'lucide-vue-next'
+import { onMounted, shallowRef, nextTick, inject, ref, type ShallowRef, type Ref } from 'vue'
+import { useNodeStore } from '@/stores/nodes'
+import { useEdgeStore } from '@/stores/edges'
+import type { Layouts } from 'v-network-graph'
+import { RandomGraphJS } from '../../../wailsjs/go/services/Randomizer'
+
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import Tooltip from './Tooltip.vue'
+import { CirclePlusIcon, PanelRightClose, PanelRightOpen, ShuffleIcon, SplineIcon, StopCircleIcon } from 'lucide-vue-next'
+
+import NodeTable from './NodeTable.vue'
+
+const { getNodes, setNodes } = useNodeStore();
+const { getEdges, setEdges } = useEdgeStore();
 
 const sidePanel = shallowRef<HTMLElement | null>(null)
 const panelWidth = shallowRef<number>(384)
 const isExpanded = shallowRef<boolean>(false)
 
+const isAddingNode = inject<ShallowRef<boolean>>('isAddingNode', shallowRef<boolean>(false));
+
+const layouts = (inject<Ref<Layouts>>('layouts', ref<Layouts>({ nodes: {} })))
+
+async function createRandomGraph() {
+  const generatedGraph = await RandomGraphJS("node", 10, 14, 500, 300);
+  setNodes(generatedGraph.nodes);
+  setEdges(generatedGraph.edges);
+  layouts.value = generatedGraph.layouts;
+  // console.log(getNodes)
+  // console.log(getEdges)
+}
+
 onMounted(async () => {
   await nextTick()
   if (sidePanel.value) {
     panelWidth.value = sidePanel.value.getBoundingClientRect().width
-    console.log(`Side panel width: ${panelWidth.value}px`)
   }
 });
 </script>
@@ -31,35 +52,36 @@ onMounted(async () => {
       </Button>
 
       <div class="flex flex-col gap-2">
-        <Tooltip>
-          <template #trigger><Button variant="secondary"><CirclePlusIcon /></Button></template>
-          <template #content>Ajouter sommet</template>
-        </Tooltip>
-        <Tooltip>
-          <template #trigger><Button variant="secondary"><SplineIcon /></Button></template>
-          <template #content>Ajouter arc</template>
-        </Tooltip>
-        <Tooltip>
-          <template #trigger><Button variant="secondary" class="mt-4"><ShuffleIcon /></Button></template>
-          <template #content>Aléatoire</template>
-        </Tooltip>
+          <Button :variant="isAddingNode ? 'default' : 'secondary'" @click="isAddingNode = !isAddingNode">
+            <StopCircleIcon v-if="isAddingNode" />
+            <CirclePlusIcon v-else />
+          </Button>
+          <Button variant="secondary"><SplineIcon /></Button>
+          <Button @click="createRandomGraph()" variant="secondary" class="mt-4"><ShuffleIcon /></Button>
       </div>
 
     </div>
 
+
     <div ref="sidePanel" class="w-xs md:w-sm h-full">
       <Card class="h-full bg-background dark:bg-background rounded-md">
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle>Tableau représentatif du graphe</CardTitle>
+          <CardDescription>Arcs et sommets</CardDescription>
         </CardHeader>
         <CardContent>
-          Card Content
+          <div class="max-h-64 overflow-auto">
+            <NodeTable v-if="Object.keys(getNodes).length != 0" :nodes="getNodes" :edges="getEdges" />
+            <div v-else class="flex items-center justify-center h-12 border">
+              <code>vide...</code>
+            </div>
+          </div>
         </CardContent>
         <CardFooter>
           Card Footer
         </CardFooter>
       </Card>
     </div>
+
   </div>
 </template>
