9c775de150f4aa5e33d998a7bf216debe67a59b1
dzyanino <dzyanino@gmail.com>
2025-07-14 19:34:15 +0300

    Add : Custom tooltip component

---
+++ frontend/src/components/custom/Tooltip.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child>
+        <slot name="trigger"></slot>
+    </TooltipTrigger>
+    <TooltipContent>
+      <slot name="content"></slot>
+    </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
