70c3ad72efef3bf3957962f9d40b455f4267bae5
dzyanino <dzyanino@gmail.com>
2025-07-15 11:13:13 +0300

    Edit : shadcn-vue's `Table.vue` to be able to have a sticky header

--- frontend/src/components/ui/table/Table.vue
+++ frontend/src/components/ui/table/Table.vue
@@ -8,9 +8,9 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div data-slot="table-container" class="relative w-full overflow-auto">
+  <!-- <div data-slot="table-container" class="relative w-full overflow-auto"> -->
     <table data-slot="table" :class="cn('w-full caption-bottom text-sm', props.class)">
       <slot />
     </table>
-  </div>
+  <!-- </div> -->
 </template>
