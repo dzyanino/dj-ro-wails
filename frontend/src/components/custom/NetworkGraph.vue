3985e73123dfe3f6cdfdeadf416c14fe0a62f39c
dzyanino <dzyanino@gmail.com>
2025-07-15 18:24:25 +0300

    Feat : Edge addition with value given through dialog

--- frontend/src/components/custom/NetworkGraph.vue
+++ frontend/src/components/custom/NetworkGraph.vue
@@ -3,6 +3,8 @@ import { inject, ref, shallowRef, watch, type ComputedRef, type Ref, type Shallo
 import { useNodeStore } from '@/stores/nodes';
 import { useEdgeStore } from '@/stores/edges';
 import { VNetworkGraph, VEdgeLabel, type EventHandlers , type Instance, type Layouts, type UserConfigs } from 'v-network-graph';
+import ModalDialog from '@/components/custom/Dialogs/ModalDialog.vue';
+import NumberField from './Inputs/NumberField.vue';
 
 const { getNodes } = useNodeStore();
 const { getEdges } = useEdgeStore();
@@ -14,12 +16,15 @@ const isAddingEdge = inject<ShallowRef<boolean>>('isAddingEdge', shallowRef<bool
 
 const selectedNodes = inject<Ref<string[]>>('selectedNodes', ref<string[]>([]));
 
+const isAddingEdgeDialogOpen = inject<ShallowRef<boolean>>('isAddingEdgeDialogOpen', shallowRef<boolean>(false));
+
 const graph = ref<Instance>();
 const nodes = ref(getNodes);
 const edges = ref(getEdges);
 const layouts = (inject<Ref<Layouts>>('layouts', ref<Layouts>({ nodes: {} })));
 
 const nextNodeIndex = shallowRef<number>(Object.keys(nodes.value).length + 1);
+const nextEdgeIndex = shallowRef<number>(Object.keys(edges.value).length + 1);
 
 const eventHandlers: EventHandlers = {
     "view:click": ({ event }) => {
@@ -39,6 +44,31 @@ const eventHandlers: EventHandlers = {
 }
 const zoomLevel = shallowRef<number>(3);
 
+const edgeWeight = shallowRef<number>(1);
+
+function cancelEdgeAddition() {
+    selectedNodes.value = [];
+    edgeWeight.value = 1;
+}
+
+function addEdge() {
+    console.log(edgeWeight.value);
+    console.log(selectedNodes.value);
+    
+    const edgeId = `edge${nextEdgeIndex.value}`;
+    edges.value[edgeId] = {
+        id: edgeId,
+        source: `${selectedNodes.value[0]}`,
+        target: `${selectedNodes.value[1]}`,
+        label: edgeWeight.value.toString(),
+    };
+
+    nextEdgeIndex.value++;
+
+    selectedNodes.value = [];
+    edgeWeight.value = 1;
+}
+
 watch(() => Object.keys(nodes.value).length, (newLength) => {
     nextNodeIndex.value = newLength + 1;
 }, { immediate: true });
@@ -48,6 +78,10 @@ watch(isAddingEdge, (isIt: boolean) => {
 
   if (configs?.value && configs.value.node) configs.value.node.selectable = isIt ? 2 : false;
 });
+
+watch(selectedNodes, () => {
+    isAddingEdgeDialogOpen.value = selectedNodes.value.length == 2;
+})
 </script>
 
 <template>
@@ -60,12 +94,20 @@ watch(isAddingEdge, (isIt: boolean) => {
         :configs="configs"
         :eventHandlers="eventHandlers"
         :zoom-level="zoomLevel"
-        class="graph border rounded-lg bg-background dark:bg-background"
+        class="h-full graph border rounded-lg bg-background dark:bg-background"
     >
         <template #edge-label="{ edge, ...slotProps }">
             <v-edge-label :text="edge.label" align="center" vertical-align="above" v-bind="slotProps"></v-edge-label>
         </template>
     </v-network-graph>
+
+    <ModalDialog v-model:open="isAddingEdgeDialogOpen" :onCancel="cancelEdgeAddition" :onConfirm="addEdge">
+            <template #dialog-title>Ajouter un arc</template>
+            <template #dialog-description>Veuillez saisir le poids</template>
+            <template #dialog-content>
+                <NumberField v-model:model-value="edgeWeight" id="weight" label="Poids" :min="1" />
+            </template>
+    </ModalDialog>
 </template>
 
 <style>
