6188137522f5ce2fae1f9226914e4657750889a1
dzyanino <dzyanino@gmail.com>
2025-07-16 13:20:53 +0300

    Feat : selection of valid nodes with duplication handling
    - changed right panel to tabs

--- frontend/src/components/custom/FloatingControls.vue
+++ frontend/src/components/custom/FloatingControls.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import { onMounted, shallowRef, nextTick, inject, ref, type ShallowRef, type Ref } from 'vue'
+import { onMounted, shallowRef, nextTick, inject, ref, type ShallowRef, type Ref, computed, watch } from 'vue'
 import { useNodeStore } from '@/stores/nodes'
 import { useEdgeStore } from '@/stores/edges'
 import type { Layouts } from 'v-network-graph'
 import { RandomGraphJS } from '../../../wailsjs/go/services/Randomizer'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { CirclePlusIcon, Loader2, PanelRightClose, PanelRightOpen, ShuffleIcon, SplineIcon, StopCircleIcon, BrushCleaningIcon } from 'lucide-vue-next'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { CirclePlusIcon, Loader2, PanelRightClose, PanelRightOpen, ShuffleIcon, SplineIcon, StopCircleIcon, BrushCleaningIcon, WaypointsIcon, RouteIcon } from 'lucide-vue-next'
 import NodeTable from './NodeTable.vue'
 import ModalDialog from './Dialogs/ModalDialog.vue'
+import Select from './Selects/Select.vue'
 
 const { getNodes, setNodes, clearNodes } = useNodeStore();
 const { getEdges, setEdges, clearEdges } = useEdgeStore();
@@ -21,13 +23,39 @@ const isAddingNode = inject<ShallowRef<boolean>>('isAddingNode', shallowRef<bool
 const isAddingEdge = inject<ShallowRef<boolean>>('isAddingEdge', shallowRef<boolean>(false));
 const isCreatingRandomGraph = shallowRef<boolean>(false);
 const isClearingGraph = shallowRef<boolean>(false);
+const isClearingGraphDialogOpen = shallowRef<boolean>(false);
 
 const selectedNodes = inject<Ref<string[]>>('selectedNodes', ref<string[]>([]));
 const selectedEdges = inject<Ref<string[]>>('selectedEdges', ref<string[]>([]));
 
 const layouts = (inject<Ref<Layouts>>('layouts', ref<Layouts>({ nodes: {} })));
 
-const isClearingGraphDialogOpen = shallowRef<boolean>(false);
+const selectedStart = shallowRef<string>('');
+const selectedEnd = shallowRef<string>('');
+
+const resolutionValidNodes = computed(() => {
+  if (Object.keys(getEdges).length != 0) {
+    const connectedNodeIds = new Set<string>()
+
+    Object.values(getEdges).forEach(edge => {
+      connectedNodeIds.add(edge.source)
+      connectedNodeIds.add(edge.target)
+    });
+
+    return Object.values(getNodes).filter(node => connectedNodeIds.has(node.id));
+  }
+
+  return [];
+});
+
+const filteredStartNodes = computed(() =>
+  resolutionValidNodes.value.filter(node => node.id !== selectedEnd.value)
+);
+
+const filteredEndNodes = computed(() =>
+  resolutionValidNodes.value.filter(node => node.id !== selectedStart.value)
+);
+
 
 function toggleAddingMode(mode: 'node' | 'edge') {
   const togglingNode = mode === 'node';
@@ -67,6 +95,12 @@ onMounted(async () => {
     panelWidth.value = sidePanel.value.getBoundingClientRect().width
   }
 });
+
+watch([selectedStart, selectedEnd], ([start, end]) => {
+  if (start && end && start === end) {
+    selectedEnd.value = '';
+  }
+});
 </script>
 
 <template>
@@ -103,23 +137,62 @@ onMounted(async () => {
 
 
     <div ref="sidePanel" class="w-xs md:w-sm h-full">
-      <Card class="h-full bg-card dark:bg-card rounded-md">
-        <CardHeader>
-          <CardTitle>Tableau représentatif du graphe</CardTitle>
-          <CardDescription>Arcs et sommets</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div class="max-h-96 relative overflow-auto">
-            <NodeTable v-if="Object.keys(getNodes).length != 0" :nodes="getNodes" :edges="getEdges" />
-            <div v-else class="flex items-center justify-center h-12 border">
-              <code>vide...</code>
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <p>_</p>
-        </CardFooter>
-      </Card>
+      <Tabs default-value="tableau" class="h-full">
+
+        <TabsList class="grid w-full grid-cols-2">
+          <TabsTrigger value="tableau">
+            Tableau
+          </TabsTrigger>
+          <TabsTrigger value="resolution">
+            Résolution
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="tableau">
+          <Card class="h-full">
+            <CardHeader>
+              <CardTitle>Tableau représentatif du graphe</CardTitle>
+              <CardDescription>Arcs et sommets</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div class="max-h-110 relative overflow-auto">
+                <NodeTable v-if="Object.keys(getNodes).length != 0" :nodes="getNodes" :edges="getEdges" />
+                <div v-else class="flex items-center justify-center h-12 border">
+                  <code>vide...</code>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="resolution">
+          <Card class="h-full">
+            <CardHeader>
+              <CardTitle>Résolution</CardTitle>
+              <CardDescription>Par l'algorithme de Dijkstra</CardDescription>
+            </CardHeader>
+            <CardContent class="h-1/4">
+              <div class="flex flex-col md:flex-row gap-4 justify-between">
+                <Select v-model:model-value="selectedStart" :items="filteredStartNodes" label="Début"
+                  placeholder="Choisir..." />
+                <Select v-model:model-value="selectedEnd" :items="filteredEndNodes" label="Fin"
+                  placeholder="Choisir..." />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div class="flex flex-row w-full gap-4 justify-between">
+                <Button>Chemin optimal
+                  <WaypointsIcon />
+                </Button>
+                <Button variant="secondary">Chemin maximal
+                  <RouteIcon />
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+
+      </Tabs>
     </div>
 
   </div>
