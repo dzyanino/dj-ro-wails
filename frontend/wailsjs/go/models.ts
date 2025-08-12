6503ed7297b685cddb43535ec483f74ed3ba41bf
dzyanino <dzyanino@gmail.com>
2025-08-12 09:26:02 +0300

    Add : wails generated bindings for Dijkstra

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -46,6 +46,96 @@ export namespace types {
 		    return a;
 		}
 	}
+	export class NodeProps {
+	    weightTo: number;
+	    previousNode: string;
+	    marked: boolean;
+	    valid: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new NodeProps(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.weightTo = source["weightTo"];
+	        this.previousNode = source["previousNode"];
+	        this.marked = source["marked"];
+	        this.valid = source["valid"];
+	    }
+	}
+	export class ResolutionNode {
+	    name?: string;
+	    properties?: Record<string, any>;
+	    id: string;
+	    nodePropsList: Record<number, NodeProps>;
+	
+	    static createFrom(source: any = {}) {
+	        return new ResolutionNode(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.name = source["name"];
+	        this.properties = source["properties"];
+	        this.id = source["id"];
+	        this.nodePropsList = this.convertValues(source["nodePropsList"], NodeProps, true);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class StepResult {
+	    nodeArray: ResolutionNode[];
+	    markedNodes: string[];
+	    currentNode: string;
+	    finished: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new StepResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.nodeArray = this.convertValues(source["nodeArray"], ResolutionNode);
+	        this.markedNodes = source["markedNodes"];
+	        this.currentNode = source["currentNode"];
+	        this.finished = source["finished"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 
 }
 
